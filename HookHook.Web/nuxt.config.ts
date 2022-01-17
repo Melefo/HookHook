35bf62d2c05e6d36aa6fe38a63de0491dbcd6eff
Gabriel <gabriel.griffin@epitech.eu>
2022-01-17 11:32:34 +0100

    Web - U/C: added tailwind css

--- HookHook.Web/nuxt.config.ts
+++ HookHook.Web/nuxt.config.ts
@@ -1,6 +1,18 @@
 import { defineNuxtConfig } from 'nuxt3'
 
-// https://v3.nuxtjs.org/docs/directory-structure/nuxt.config
 export default defineNuxtConfig({
-
+    nitro: {
+        preset: 'browser'
+    },
+    css: [
+        "@/assets/css/main.css",
+    ],
+    typescript: {
+        strict: true
+    },
+    build: {
+        postcss: {
+            postcssOptions: require("./postcss.config.js"),
+        },
+    }
 })
