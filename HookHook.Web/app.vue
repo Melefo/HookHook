ef05747c1df6b66941d3cfa64fe8fd997a815e17
Gabriel <gabriel.griffin@epitech.eu>
2022-01-17 16:12:36 +0100

    Web - U: found how to toggle data in vue

--- HookHook.Web/app.vue
+++ HookHook.Web/app.vue
@@ -1,5 +1,17 @@
 <template>
-	<div>
-		<NuxtPage/>
-	</div>
+    <Html v-bind:class="{dark: darkMode}">
+        <input type="checkbox" v-model="darkMode">
+        <NuxtPage/>
+    </Html>
 </template>
+
+<script>
+
+    export default {
+        data() {
+            return {
+                darkMode: true
+            }
+        }
+    }
+</script>
--- HookHook.Web/pages/index.vue
+++ HookHook.Web/pages/index.vue
@@ -1,5 +1,7 @@
 <template>
-    <h1 class="text-3xl font-bold underline">
-        Hello world!
-    </h1>
+    <div class="bg-white dark:bg-black">
+        <h1 class="dark:text-white text-3xl font-bold underline">
+            Hello world!
+        </h1>
+    </div>
 </template>
