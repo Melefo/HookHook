d6302151f78862d90b7be2837ea52156ffc19ea3
Melefo <melefo@melefo.xyz>
2022-01-18 15:55:01 +0100

    Switch Nuxt to Vue

--- HookHook.Web/components/Navbar.vue
+++
@@ -1,5 +0,0 @@
-<template>
-    <h1 class="font-comfortaa dark:text-white ml-10 text-3xl font-bold">
-        HookHook
-    </h1>
-</template>
--- HookHook.Web/tailwind.config.js
+++ HookHook.Web/tailwind.config.js
@@ -1,18 +1,10 @@
 module.exports = {
-    content: [
-        "./components/**/*.{js,vue,ts}",
-        "./layouts/**/*.vue",
-        "./pages/**/*.vue",
-        "./plugins/**/*.{js,ts}",
-        "./nuxt.config.{js,ts}",
-    ],
-    darkMode: 'class',
-    theme: {
-        extend: {
-            fontFamily: {
-                'comfortaa': 'Comfortaa'
-            }
-        },
-    },
-    plugins: [],
+  content: [
+    "./index.html",
+    "./src/**/*.{vue,js,ts,jsx,tsx}",
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
 }
