be08b561b1600dab79bcacdf4e91d5ecad2aabf3
Melefo <melefo@melefo.xyz>
2022-01-20 23:01:59 +0100

    Stylized web home/login page

---
+++ HookHook.Web/src/components/BlocComponent.vue
@@ -0,0 +1,5 @@
+<template>
+    <div class="rounded-xl bg-[#3B3F43] p-5">
+        <slot />
+    </div>
+</template>
--- HookHook.Web/src/views/HomeView.vue
+++ HookHook.Web/src/views/HomeView.vue
@@ -1,24 +1,70 @@
 
 <template>
-  <div class="bg-[#F9F9F9] dark:bg-[#181A1E] fixed h-screen w-screen p-10">
-    <div class="flex justify-between">
-      <div>
-        <h1 class="font-bold text-5xl text-black dark:text-white">
-          H<span class="text-[#F09113] inline-block">oo<img class="m-auto mt-[-10px]" :src="Orange" /></span>kH<span class="text-[#A3E7EE]">oo</span>k
-        </h1>
-      </div>
-      <div>
+  <div class="transition-colors duration-200 bg-[#F9F9F9] dark:bg-[#181A1E] h-screen w-screen p-10">
+    <header class="flex flex-col sm:flex-row justify-between">
+      <RouterLink to="/">
+        <HookHook class="text-5xl" />
+      </RouterLink>
+      <div class="mt-6 sm:mt-0">
         <Switch @ToggleDarkMode="this.$emit('ToggleDarkMode')" />
       </div>
+    </header>
+    <section>
+      <h2 class="my-5 sm:my-10 font-medium text-3xl text-black dark:text-white transition-colors duration-200">Welcome to <HookHook />, the best way to automate your work and make it more relaxing.</h2>
+    </section>
+    <div class="grid grid-rows-none	grid-rows-2 xl:grid-cols-2 gap-10 xl:gap-40 xl:mx-40">
+      <Bloc class="flex flex-col justify-center items-center">
+        <form class="flex flex-col">
+          <Input class="pb-5" type="text" label="Username/Email" reverse="true" />
+          <Input class="pb-5" type="password" label="Password" reverse="true" />
+          <Input type="submit" value="Se connecter" reverse="true" />
+        </form>
+        <div class="grid grid-cols-3 sm:grid-cols-6 gap-4 mt-8">
+          <img class="h-10" alt="google" src="@/assets/img/google.svg" />
+          <img class="h-10" alt="twitch" src="@/assets/img/twitch.svg" />
+          <img class="h-10" alt="github" src="@/assets/img/github.svg" />
+          <img class="h-10" alt="spotify" src="@/assets/img/spotify.svg" />
+          <img class="h-10" alt="twitter" src="@/assets/img/twitter.svg" />
+          <img class="h-10" alt="discord" src="@/assets/img/discord.svg" />
+        </div>
+      </Bloc>
+      <Bloc class="flex justify-center items-center">
+        <form class="flex flex-col">
+          <div class="grid grid-cols-2 gap-4">
+            <Input class="pb-5" type="text" label="First name" reverse="true" />
+            <Input class="pb-5" type="text" label="Last name" reverse="true" />
+          </div>
+          <Input class="pb-5" type="email" label="Email" reverse="true" />
+          <Input class="pb-5" type="text" label="Username" reverse="true" />
+          <div class="grid grid-cols-2 gap-4">
+            <Input class="pb-5" type="password" label="Password" reverse="true" />
+            <Input class="pb-5" type="password" label="Confirm password" reverse="true" />
+          </div>
+          <Input type="submit" value="S'enregistrer" reverse="true" />
+        </form>
+      </Bloc>
+    </div>
+    <div class="flex flex-row justify-evenly mt-20">
+      <img class="object-contain w-52 h-52" src="@/assets/pinguin/breakdance.gif" />
+      <img class="object-contain w-52 h-52" src="@/assets/pinguin/dance.gif" />
+      <img class="object-contain w-52 h-52" src="@/assets/pinguin/clap.gif" />
+      <img class="object-contain w-52 h-52" src="@/assets/pinguin/mop.gif" />
+      <img class="object-contain w-52 h-52" src="@/assets/pinguin/maracas.gif" />
+      <img class="object-contain w-52 h-52" src="@/assets/pinguin/warp.gif" />
+      <img class="object-contain w-52 h-52" src="@/assets/pinguin/photo.gif" />
     </div>
   </div>
 </template>
 
 <script lang="ts">
 import Switch from "@/components/SwitchComponent.vue";
-import { defineComponent } from 'vue'
+import Input from "@/components/InputComponent.vue";
+import Bloc from "@/components/BlocComponent.vue";
+import HookHook from "@/components/HookHookComponent.vue";
+
+import { defineComponent } from "vue";
 
 export default defineComponent({
-  components: { Switch },
+  components: { Switch, Input, Bloc , HookHook },
 });
 </script>
