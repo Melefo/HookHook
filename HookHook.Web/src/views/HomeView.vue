15da59383aa260c39d7518701ed56cb5ee229a8d
Nywes <Nywes@localhost.localdomain>
2022-01-27 15:45:51 +0100

    push pingu gifs for homepage

--- HookHook.Web/src/views/HomeView.vue
+++ HookHook.Web/src/views/HomeView.vue
@@ -1,14 +1,20 @@
 <template>
-  <div class="text-white mt-14">
-    <p>Welcome!</p>
-    <router-link v-if="!isLoggedIn" to="/login">
-      <p>Login</p>
-    </router-link>
-    <router-link v-else to="/dashboard">
-      <p>Dashboard</p>
-    </router-link>
+  <div>
+    <div class="text-black mt-14 dark:text-white">
+      <p>Welcome!</p>
+      <router-link v-if="!isLoggedIn" to="/login">
+        <p>Login</p>
+      </router-link>
+      <router-link v-else to="/dashboard">
+        <p>Dashboard</p>
+      </router-link>
+    </div>
+    <div class="text-black mt-14 dark:text-white">
+      <img class="object-contain w-140 h-140 absolute bottom-10 left-10" src="@/assets/pinguin/Pingu1.gif" />
+      <img class="object-contain w-140 h-140 absolute bottom-50 left-50 scale-x-[-1]" src="@/assets/pinguin/Pingu1.gif" />
+    </div>
   </div>
-</template>
+  </template>
 
 <script>
 import { mapGetters } from 'vuex';
