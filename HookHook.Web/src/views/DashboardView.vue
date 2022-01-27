b6ec54d0469c01c8d268ad315441a36e8f6cee84
Gabriel <gabriel.griffin@epitech.eu>
2022-01-27 16:50:34 +0100

    Web - U: advanced dashboard design

--- HookHook.Web/src/views/DashboardView.vue
+++ HookHook.Web/src/views/DashboardView.vue
@@ -12,24 +12,33 @@
         </h2>
 
         <div class="flex gap-x-5 mt-8">
-            <div class="rounded-md w-32 h-32 bg-[#A3E7EE]">
+            <div class="rounded-lg w-32 h-32 bg-[#A3E7EE]">
                 <img class="mx-auto mt-3.5 w-24 h-24" alt="twitter" src="@/assets/img/twitter.svg" />
             </div>
-            <div class="rounded-md w-32 h-32 bg-[#B4E1DC]">
+            <div class="rounded-lg w-32 h-32 bg-[#B4E1DC]">
                 <img class="mx-auto mt-3.5 w-24 h-24" alt="spotify" src="@/assets/img/spotify.svg" />
             </div>
-            <div class="rounded-md w-32 h-32 bg-[#D9D1EA]">
+            <div class="rounded-lg w-32 h-32 bg-[#D9D1EA]">
                 <img class="mx-auto mt-3.5 w-24 h-24" alt="discord" src="@/assets/img/discord.svg" />
             </div>
-            <div class="rounded-md w-32 h-32 bg-[#F5CDCB]">
+            <div class="rounded-lg w-32 h-32 bg-[#F5CDCB]">
                 <img class="mx-auto mt-3.5 w-24 h-24" alt="github" src="@/assets/img/github.svg" />
             </div>
-            <div class="rounded-md w-32 h-32 bg-[#F8CBAA]">
+            <div class="rounded-lg w-32 h-32 bg-[#F8CBAA]">
                 <img class="mx-auto mt-3.5 w-24 h-24" alt="youtube" src="@/assets/img/youtube.svg" />
             </div>
-            <div class="rounded-md w-32 h-32 bg-[#FFFFC7]">
+            <div class="rounded-lg w-32 h-32 bg-[#FFFFC7]">
                 <img class="mx-auto mt-3.5 w-24 h-24" alt="twitch" src="@/assets/img/twitch.svg" />
             </div>
+        </div>
+
+        <h2 class="text-2xl mt-10 text-black dark:text-white transition-colors duration-200">
+            Creator
+        </h2>
+        <div class="rounded-lg w-1/3 h-96 bg-[#3B3F43]">
+            <h2 class="text-2xl ml-5 text-black dark:text-white transition-colors duration-200">
+                AREA name:
+            </h2>
         </div>
     </div>
 </template>
