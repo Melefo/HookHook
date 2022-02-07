debf8d5e1f45d8ab42b1f32b866c6278c52b4d62
mllusar <mathis.llusar@epitech.eu>
2022-02-07 14:43:00 +0100

    Adding button for Cretor

--- HookHook.Mobile/lib/widgets/areas_creator.dart
+++ HookHook.Mobile/lib/widgets/areas_creator.dart
@@ -2,9 +2,16 @@ import 'package:flutter/material.dart';
 import 'package:mvc_application/controller.dart';
 import 'package:mvc_application/view.dart';
 
-class AreasCreator extends StatelessWidget {
+class AreasCreator extends StatefulWidget {
+  @override
+  _AreasCreator createState() => _AreasCreator();
+}
 
-  const AreasCreator({Key? key}) : super(key: key);
+class _AreasCreator extends State<AreasCreator> {
+
+  String dropdownvalue = 'Tweet';
+
+  var items =  ['Tweet', 'Push', 'Like', 'Message Pinned'];
 
   @override
   Widget build(BuildContext context) {
@@ -18,39 +25,47 @@ class AreasCreator extends StatelessWidget {
             children: [
               Text('Your Area name :', style: TextStyle(color: Colors.white)),
               TextField(),
-              Padding(
-                padding: const EdgeInsets.all(20.0),
-                child: Row(
-                  children: [
-                    Text('When : ', style: TextStyle(color: Colors.white)),
-                    DropdownButton<String>(
-                      items: <String>['Tweet', 'Push', 'Like', 'Message Pinned'].map((String value) {
-                        return DropdownMenuItem<String>(
-                          value: value,
-                          child: Text(value),
-                        );
-                      }).toList(),
-                      onChanged: (_) {},
-                    ),
-                  ],
-                ),
+              Row(
+                children: [
+                  Text('When : ', style: TextStyle(color: Colors.white)),
+                  DropdownButton<String>(
+                    items: <String>['Tweet', 'Push', 'Like', 'Message Pinned'].map((String value) {
+                      return DropdownMenuItem<String>(
+                        value: value,
+                        child: Text(value),
+                      );
+                    }).toList(),
+                    onChanged: (_) {},
+                  ),
+                ],
               ),
-              Padding(
-                padding: const EdgeInsets.all(20.0),
-                child: Row(
-                  children: [
-                    Text('Do : ', style: TextStyle(color: Colors.white)),
-                    DropdownButton<String>(
-                      items: <String>['Tweet', 'Push', 'Like', 'Message Pinned'].map((String value) {
-                        return DropdownMenuItem<String>(
-                          value: value,
-                          child: Text(value),
-                        );
-                      }).toList(),
-                      onChanged: (_) {},
-                    ),
-                  ],
-                ),
+              Row(
+                children: [
+                  Text('Do : ', style: TextStyle(color: Colors.white)),
+                  DropdownButton(
+                    value: dropdownvalue,
+                    icon: Icon(Icons.keyboard_arrow_down),
+                    items:items.map((String items) {
+                      return DropdownMenuItem(
+                          value: items,
+                          child: Text(items)
+                      );
+                    }
+                    ).toList(),
+                    onChanged: (String newValue){
+                      setState(() {
+                        dropdownvalue = newValue;
+                      });
+                    },
+                  ),
+                ],
+              ),
+              ElevatedButton.icon(
+                onPressed: () {
+                  // Respond to button press
+                },
+                icon: Icon(Icons.add, size: 18),
+                label: Text("CONTAINED BUTTON"),
               )
             ],
           ),
