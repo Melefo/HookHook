e4e50a62c6c4e3d4ce2d585ae46cfc8af97a222a
Gabriel <gabriel.griffin@epitech.eu>
2022-02-08 16:39:21 +0100

    Web - U: using old-school requests to login with google

--- HookHook.Web/src/components/OAuth/GoogleOAuthComponent.vue
+++ HookHook.Web/src/components/OAuth/GoogleOAuthComponent.vue
@@ -18,21 +18,47 @@ export default defineComponent({
   methods: {
     ...mapActions("user", ["google"]),
     async handleGoogle() {
+      window.removeEventListener("message", this.receiveGoogle);
 
-        try {
-            // console.log(Vue3GoogleOauth)
-            // const code = await this.$gAuth.getAuthCode();
-            // const { error, errors } = await this.google(code);
-            // this.error = error;
-            // this.errors = errors;
-            // if (!this.error && !this.errors) {
-            //     this.$router.push("/dashboard");
-            // }
-        } catch {
-            console.error("Problemo largo con el google");
-            // this.error = "Error with Google OAuth";
-        }
-    }
+      let scopes = "openid";
+      scopes += " email";
+      scopes += " profile";
+
+      const url = `https://accounts.google.com/o/oauth2/v2/auth?client_id=${
+        process.env.VUE_APP_GOOGLE_CLIENTID
+      }&redirect_uri=${
+        window.location.origin + '/oauth'
+      }&state=${Math.random().toString(36).slice(2)}
+      &response_type=code
+      &scope=${scopes}
+      &nonce=${Math.random().toString(36).slice(2)}`;
+      let popup = window.open(
+        url,
+        "_blank",
+        "width=500, height=750, left=20, top=20, popup=true"
+      );
+      if (popup == null) {
+        return;
+      }
+      popup.focus();
+      window.addEventListener("message", this.receiveGoogle, false);
+    },
+    async receiveGoogle(event: any) {
+      if (event.origin !== window.location.origin) {
+        return;
+      }
+      let data = Object.fromEntries(new URLSearchParams(event.data));
+      if (!data.code) {
+        return;
+      }
+      window.removeEventListener("message", this.receiveGoogle);
+      const { errors, error } = await this.google(data.code);
+      this.errors = errors || null;
+      this.error = error || null;
+      if (!this.error && !this.errors) {
+        this.$router.push("/dashboard");
+      }
+    },
   },
 });
 </script>
--- HookHook.Web/src/main.ts
+++ HookHook.Web/src/main.ts
@@ -5,12 +5,8 @@ import store from './store'
 import './index.css'
 import Default from '@/layouts/defaultLayout.vue'
 import Title from '@/layouts/titleLayout.vue'
-// import GAuth from 'vue3-google-oauth2'
 
 const app = createApp(App)
-
-// const gAuthOptions = { clientId: process.env.VUE_APP_GOOGLE_CLIENTID, scope: 'email', prompt: 'consent', fetch_basic_profile: false }
-// app.use(GAuth, gAuthOptions);
 
 app.use(store).use(router).mount('#app')
 app.component('DefaultLayout', Default)
