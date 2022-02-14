2ff950501166eee9a333965a27e3b98f3867da91
Nywes <Nywes@localhost.localdomain>
2022-02-14 16:24:07 +0100

    DropdownComponent.vue -M odifing the dropdown section in the area creator

--- HookHook.Web/src/components/DialogComponent.vue
+++ HookHook.Web/src/components/DialogComponent.vue
@@ -1,41 +1,43 @@
 <template>
-  <button type="button" class="button" :style="{ 'background-color': bgColor }" @click="openModal">
-    <img class="w-20 h-20 m-auto" :src="require(`@/assets/img/coloredsvg/${src}`)"/>
-  </button>
-  <TransitionRoot appear :show="IsOpen" as="template">
-    <Dialog as="div" @close="closeModal">
-      <div class="fixed inset-0 z-10 overflow-y-auto">
-        <div class="min-h-screen px-4 text-center">
-          <span class="inline-block h-screen align-middle" aria-hidden="true"/>
-          <TransitionChild as="template">
-            <div
-              :class="`inline-block w-full max-w-[35%] h-[400px] p-4 my-8 overflow-hidden text-center align-middle transition-all transform shadow-xl rounded-2xl`"
-              :style="{ 'background-color': bgColor }"
-            >
-              <DialogTitle as="h3" class=" text-lg font-bold leading-6 text-grey-800">
-                {{ text }}
-              </DialogTitle>
-              <div class="mt-2">
-                <p class="text-sm text-gray-500">
-                  lorem ipsum
-                </p>
-              </div>
+  <div>
+    <button type="button" class="button" :style="{ 'background-color': bgColor }" @click="openModal">
+      <img class="w-20 h-20 m-auto" :src="require(`@/assets/img/coloredsvg/${src}`)"/>
+    </button>
+    <TransitionRoot appear :show="IsOpen" as="template">
+      <Dialog as="div" @close="closeModal">
+        <div class="fixed inset-0 z-10 overflow-y-auto">
+          <div class="min-h-screen px-4 text-center">
+            <span class="inline-block h-screen align-middle" aria-hidden="true"/>
+            <TransitionChild as="template">
+              <div
+                :class="`inline-block w-full max-w-[35%] h-[400px] p-4 my-8 overflow-hidden text-center align-middle transition-all transform shadow-xl rounded-2xl`"
+                :style="{ 'background-color': bgColor }"
+              >
+                <DialogTitle as="h3" class=" text-lg font-bold leading-6 text-grey-800">
+                  {{ text }}
+                </DialogTitle>
+                <div class="mt-2">
+                  <p class="text-sm text-gray-500">
+                    lorem ipsum
+                  </p>
+                </div>
 
-              <div class="mt-4">
-                <button
-                  type="button"
-                  class="inline-flex justify-center px-4 py-2 text-sm font-medium text-black bg-neutral-200 border border-transparent rounded-md hover:bg-neutral-300 focus:outline-none focus-visible:ring-2 focus-visible:ring-offset-2 focus-visible:ring-neutral-500"
-                  @click="closeModal"
-                >
-                  Close this popup
-                </button>
+                <div class="mt-4">
+                  <button
+                    type="button"
+                    class="inline-flex justify-center px-4 py-2 text-sm font-medium text-black bg-neutral-200 border border-transparent rounded-md hover:bg-neutral-300 focus:outline-none focus-visible:ring-2 focus-visible:ring-offset-2 focus-visible:ring-neutral-500"
+                    @click="closeModal"
+                  >
+                    Close this popup
+                  </button>
+                </div>
               </div>
-            </div>
-          </TransitionChild>
+            </TransitionChild>
+          </div>
         </div>
-      </div>
-    </Dialog>
-  </TransitionRoot>
+      </Dialog>
+    </TransitionRoot>
+  </div>
 </template>
 
 <script lang="ts">
