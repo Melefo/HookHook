73f307801fed5af991afbec08705d03d561c8563
Melefo <melefo@melefo.xyz>
2022-02-28 22:10:31 +0100

    Finishing login page

--- HookHook.Mobile/lib/views/login.dart
+++ HookHook.Mobile/lib/views/login.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:hookhook/adaptive_state.dart';
 import 'package:hookhook/hookhook_colors.dart';
 import 'package:hookhook/services_icons.dart';
+import 'package:hookhook/widgets/welcome_hookhook.dart';
 import 'package:hookhook/wrapper/backend.dart';
 
 class LoginView extends StatefulWidget {
@@ -25,72 +26,132 @@ class _LoginView extends AdaptiveState<LoginView> {
   @override
   Widget build(BuildContext context) =>
       Scaffold(
-          body: Padding(
-            padding: const EdgeInsets.symmetric(
-                horizontal: 16
-            ),
-            child: Column(
-              children: [
-                const Padding(
-                    padding: EdgeInsets.only(
-                        top: 79
-                    )
-                ),
-                TextFormField(
-                    decoration: InputDecoration(
-                        border: UnderlineInputBorder(),
-                        labelText: "Username/Email"
-                    )
-                ),
-                TextFormField(
-                    decoration: InputDecoration(
-                        border: UnderlineInputBorder(),
-                        labelText: "Password"
-                    )
-                ),
-                TextButton(
-                  onPressed: () => {},
-                  child: Text("Forgot password?"),
-                  style: ButtonStyle(
-                    backgroundColor: MaterialStateProperty.all(Colors.white),
-                  ),
-                ),
-                TextFormField(
-                  decoration: InputDecoration(
-                      border: UnderlineInputBorder(),
-                      labelText: "HookHook Instance",
-                  ),
-                  initialValue: Backend.apiEndpoint,
-                  onChanged: (text) async {
-                    Backend.apiEndpoint = text;
-                    await Backend.init();
-                  },
-                ),
-                TextButton(
-                    onPressed: () => {},
-                    child: Text("Login")
-                ),
-                TextButton(
-                    onPressed: () => {},
-                    child: Text("Register"),
-                    style: ButtonStyle(
+        backgroundColor: darkMode ? HookHookColors.dark : HookHookColors.light,
+          body: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Row(
+                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                children: [
+                  Image.asset("assets/pinguin/warp.gif", height: 100, width: 100),
+                  WelcomeHookHook(),
+                  Image.asset("assets/pinguin/warp.gif", height: 100, width: 100)
+                ],
+              ),
+              Padding(
+                padding: const EdgeInsets.symmetric(horizontal: 92),
+                child: Column(
+                  children: [
+                    TextFormField(
+                        decoration: InputDecoration(
+                            border: UnderlineInputBorder(),
+                            labelText: "Username/Email"
+                        )
                     ),
+                    TextFormField(
+                        decoration: InputDecoration(
+                            border: UnderlineInputBorder(),
+                            labelText: "Password"
+                        )
+                    ),
+                    TextButton(
+                      onPressed: () => {},
+                      child: Text(
+                        "Forgot password?",
+                        style: TextStyle(
+                            color: darkMode ? Colors.white : Colors.black,
+                            decoration: TextDecoration.underline
+                        ),
+                      ),
+                    ),
+                    TextFormField(
+                      decoration: InputDecoration(
+                        border: UnderlineInputBorder(),
+                        labelText: "HookHook Instance",
+                      ),
+                      initialValue: Backend.apiEndpoint,
+                      onChanged: (text) async {
+                        Backend.apiEndpoint = text;
+                        await Backend.init();
+                      },
+                    ),
+                    const Padding(
+                      padding: EdgeInsets.all(8),
+                    ),
+                    TextButton(
+                        onPressed: () => {},
+                        child: Text(
+                          "Login",
+                          style: TextStyle(
+                              color: darkMode ? Colors.white : Colors.black
+                          ),
+                        ),
+                        style: ButtonStyle(
+                            backgroundColor: MaterialStateProperty.all(
+                                darkMode ? HookHookColors.gray : Colors.white
+                            ),
+                            padding: MaterialStateProperty.all(
+                                const EdgeInsets.all(15)),
+                            shape: MaterialStateProperty.all(
+                                RoundedRectangleBorder(
+                                    borderRadius: BorderRadius.circular(10)
+                                )
+                            ),
+                          minimumSize: MaterialStateProperty.all(Size(150, 0))
+                        )
+                    ),
+                    const Padding(
+                      padding: EdgeInsets.all(4),
+                    ),
+                    TextButton(
+                        onPressed: () => {},
+                        child: Text(
+                          "Register",
+                          style: TextStyle(
+                              color: darkMode ? Colors.white : Colors.black
+                          ),
+                        ),
+                        style: ButtonStyle(
+                            backgroundColor: MaterialStateProperty.all(
+                                darkMode ? HookHookColors.gray : Colors.white
+                            ),
+                            padding: MaterialStateProperty.all(
+                                const EdgeInsets.all(15)),
+                            shape: MaterialStateProperty.all(
+                                RoundedRectangleBorder(
+                                    borderRadius: BorderRadius.circular(10)
+                                )
+                            ),
+                            minimumSize: MaterialStateProperty.all(Size(150, 0))
+                        )
+                    ),
+                  ],
                 ),
-                Row(
-                  children: generateFromServices(),
-                  mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+              ),
+              const Padding(
+                padding: EdgeInsets.all(8),
+              ),
+              Row(
+                children: generateFromServices(),
+                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+              ),
+              /*TextButton(
+                onPressed: () {
+                  setDarkMode(!darkMode);
+                },
+                child: Text(
+                  darkMode ? 'Passer au light mode' : "Passer au dark mode",
+                  style: TextStyle(
+                      color: darkMode ? Colors.black : Colors.white
+                  )
                 ),
-                TextButton(
-                  onPressed: () {
-                    setDarkMode(!darkMode);
-                  },
-                  child: Text(darkMode ? 'Passer au light mode' : "Passer au dark mode", style: TextStyle(color: darkMode ? Colors.black : Colors.white),),
-                  style: ButtonStyle(
-                    backgroundColor: MaterialStateProperty.all(darkMode ? HookHookColors.blue : HookHookColors.orange)
-                  ),
-                )
-              ],
-            ),
+                style: ButtonStyle(
+                    backgroundColor: MaterialStateProperty.all(
+                        darkMode ? HookHookColors.blue : HookHookColors
+                            .orange)
+                ),
+              )*/
+            ],
           )
       );
 }
