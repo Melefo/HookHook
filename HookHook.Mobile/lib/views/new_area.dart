9b8819fa2434cceb8858cc8e359dc4226dba402a
mllusar <mathis.llusar@epitech.eu>
2022-03-09 18:26:50 +0100

    Working Creator

--- HookHook.Mobile/lib/views/new_area.dart
+++ HookHook.Mobile/lib/views/new_area.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:hookhook/models/action_parameters.dart';
 import 'package:hookhook/models/create_area_model.dart' as create;
 import 'package:hookhook/models/service_info_model.dart';
@@ -71,108 +72,123 @@ class _NewAreaView extends AdaptiveState<NewAreaView> {
                     ),
                   ),
                 ),
-                SingleChildScrollView(
-                  child: SizedBox(
-                    height: 600,
-                    child: Column(
-                      children: [
-                        Padding(
-                          padding: const EdgeInsets.all(10.0),
-                          child: TextFormField(
-                            decoration: const InputDecoration(
-                                border: UnderlineInputBorder(),
-                                labelText: "Area Name"
-                            ),
-                            controller: areaName,
-                          ),
-                        ),
-
-                        /// ACTION PART
-                        Padding(
-                          padding: const EdgeInsets.only(top: 20.0),
-                          child: Align(
-                              alignment: Alignment.centerLeft,
-                              child: Text(
-                                "When :",
-                                style: TextStyle(
-                                    color: darkMode
-                                        ? HookHookColors.blue
-                                        : HookHookColors.orange
-                                ),
-                              )
-                          ),
-                        ),
-                        Creator(
-                          areaType: AreaType.action,
-                          services: services,
-                          action: action,
-                          onUpdate: () {
-                            setState(() {
-
-                            });
-                          },
-                        ),
-
-                        ///REACTION PART
-                        Column(
+                Padding(
+                  padding: const EdgeInsets.only(top: 15.0),
+                  child: Container(
+                    padding: const EdgeInsets.all(8.0),
+                    decoration: BoxDecoration(
+                      borderRadius: const BorderRadius.all(
+                          Radius.circular(25)
+                      ),
+                      border: Border.all(
+                        width: 3.0,
+                        color: darkMode ? Colors.white : HookHookColors.dark
+                      )
+                    ),
+                    child: SizedBox(
+                      height: 0.69.sh,
+                      child: SingleChildScrollView(
+                        child: Column(
                           children: [
-                            const Divider(),
                             Padding(
-                              padding: const EdgeInsets.only(top: 25.0),
+                              padding: const EdgeInsets.all(10.0),
+                              child: TextFormField(
+                                decoration: const InputDecoration(
+                                    border: UnderlineInputBorder(),
+                                    labelText: "Area Name"
+                                ),
+                                controller: areaName,
+                              ),
+                            ),
+
+                            /// ACTION PART
+                            Padding(
+                              padding: const EdgeInsets.only(top: 20.0),
                               child: Align(
                                   alignment: Alignment.centerLeft,
                                   child: Text(
-                                    "Do :",
+                                    "When :",
                                     style: TextStyle(
-                                        color: darkMode ? HookHookColors
-                                            .blue : HookHookColors.orange
+                                        color: darkMode
+                                            ? HookHookColors.blue
+                                            : HookHookColors.orange
                                     ),
                                   )
                               ),
                             ),
-                          ],
-                        ),
-                        for (int i = 0; i < reactions.length; i++)
-                          Creator(
-                              areaType: AreaType.reaction,
+                            Creator(
+                              areaType: AreaType.action,
                               services: services,
-                              action: reactions[i],
+                              action: action,
                               onUpdate: () {
                                 setState(() {
 
                                 });
                               },
-                              onDelete: i == 0 ? null : () {
+                            ),
+
+                            ///REACTION PART
+                            Column(
+                              children: [
+                                const Divider(),
+                                Padding(
+                                  padding: const EdgeInsets.only(top: 25.0),
+                                  child: Align(
+                                      alignment: Alignment.centerLeft,
+                                      child: Text(
+                                        "Do :",
+                                        style: TextStyle(
+                                            color: darkMode ? HookHookColors
+                                                .blue : HookHookColors.orange
+                                        ),
+                                      )
+                                  ),
+                                ),
+                              ],
+                            ),
+                            for (int i = 0; i < reactions.length; i++)
+                              Creator(
+                                areaType: AreaType.reaction,
+                                services: services,
+                                action: reactions[i],
+                                onUpdate: () {
+                                  setState(() {
+
+                                  });
+                                },
+                                onDelete: i == 0 ? null : () {
+                                  setState(() {
+                                    reactions.remove(reactions[i]);
+                                  });
+                                },
+                              ),
+                            TextButton(
+                              onPressed: () {
                                 setState(() {
-                                  reactions.remove(reactions[i]);
+                                  reactions.add(ActionParameters());
                                 });
                               },
-                          ),
-                        TextButton(
-                          onPressed: () {
-                            setState(() {
-                              reactions.add(ActionParameters());
-                            });
-                          },
-                          style: TextButton.styleFrom(
-                              backgroundColor: darkMode
-                                  ? HookHookColors.gray
-                                  : Colors.white,
-                              padding: const EdgeInsets.all(12),
-                              minimumSize: Size.zero,
-                              shape: RoundedRectangleBorder(
-                                  borderRadius: BorderRadius.circular(10)
-                              )
-                          ),
-                          clipBehavior: Clip.none,
-                          child: Icon(
-                              Icons.add,
-                              color: darkMode
-                                  ? Colors.white
-                                  : HookHookColors.gray
-                          ),
-                        )
-                      ],
+                              style: TextButton.styleFrom(
+                                  backgroundColor: darkMode
+                                      ? HookHookColors.gray
+                                      : Colors.white,
+                                  padding: const EdgeInsets.all(12),
+                                  minimumSize: Size.zero,
+                                  shape: RoundedRectangleBorder(
+                                      borderRadius: BorderRadius.circular(10)
+                                  )
+                              ),
+                              clipBehavior: Clip.none,
+                              child: Icon(
+                                  Icons.add,
+                                  color: darkMode
+                                      ? Colors.white
+                                      : HookHookColors.gray
+                              ),
+                            )
+                          ],
+                        ),
+                      ),
                     ),
                   ),
                 ),
